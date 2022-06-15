a22fc23f4cfb85a8feb1916e82a62d2c3004a86f
Young Nwadike <young@ucraft.com>
2022-06-15 19:46:54 +0400

    [CONFIGURATION] setup initial app configurations (prettier, eslint, styled-component, ant-design)

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["import", "react", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-var-requires": 0,
+    "react/jsx-uses-react": 0,
+    "react/react-in-jsx-scope": 0,
+    "no-case-declarations": 0,
+    "@typescript-eslint/ban-ts-comment": 0,
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/ban-types": 0,
+    "@typescript-eslint/camelcase": 0,
+    "@typescript-eslint/no-empty-function": 0,
+    curly: "error",
+    "react/prop-types": 0,
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "react/display-name": 0,
+    "@typescript-eslint/ban-ts-ignore": 0,
+    "@typescript-eslint/no-use-before-define": 0,
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "import/first": 1,
+    "import/newline-after-import": 1,
+    "import/no-duplicates": 1,
+    "no-prototype-builtins": 0,
+  },
+};
